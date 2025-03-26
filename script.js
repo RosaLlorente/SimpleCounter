82cef667bc56edf41852a7c2ae4ea875e22842e1
Rosa Llorente <rosamariallorentemojica@gmail.com>
2025-03-26 16:49:14 +0100

    Falta mejorar diseño,realizar bien las traducciones,terminar de documentar codigo

---
+++ script.js
@@ -0,0 +1,193 @@
+window.onload = function() {//Fallas con la asignacion de modos,se debe arreglar
+    //Gestionar el modo
+    var modo = document.getElementById("Modo");
+    var SimpleCounterdiv = document.getElementById("SimpleCounter");
+    var Cronometrodiv = document.getElementById("Cronometro");
+    var Relojdiv = document.getElementById("Reloj");
+    
+    actualizarModo(modo,SimpleCounterdiv, Cronometrodiv, Relojdiv);
+    modo.addEventListener("change", () => {
+        actualizarModo(modo,SimpleCounterdiv, Cronometrodiv, Relojdiv);
+    });
+}
+
+function actualizarModo(modo,SimpleCounterdiv, Cronometrodiv, Relojdiv) {
+    var valor = modo.value; // Obtener el valor actual del select
+
+    // Ocultar todos los divs
+    SimpleCounterdiv.style.display = "none";
+    Cronometrodiv.style.display = "none";
+    Relojdiv.style.display = "none";
+
+    // Mostrar el div correspondiente
+    if (valor === "SimpleCounter") 
+    {
+        SimpleCounterdiv.style.display = "flex";
+        SimpleCounter();
+    } 
+    else if (valor === "Cronometro") 
+    {
+        Cronometrodiv.style.display = "flex";
+        Cronometro();
+    } 
+    else if (valor === "Reloj") 
+    {
+        Relojdiv.style.display = "flex";
+        Reloj();
+    }
+}
+
+function SimpleCounter() {
+    var Incrementar = document.getElementById("Incrementar");
+    var Decrementar = document.getElementById("Decrementar");
+    var Reset = document.getElementById("Reset");
+    var Contador = document.getElementById("counter");
+
+    var Cantidad_Contador = 0;
+
+    //Gestionar el incremento del contador de clicks
+    Incrementar.addEventListener("click", function() {
+        Cantidad_Contador++;
+        Contador.innerText = Cantidad_Contador;
+        if(Cantidad_Contador === 0)
+        {
+            Contador.style.color = "var(--color-positivo)";
+        }
+        else if(Cantidad_Contador > 0)
+        {
+            Contador.style.color = "var(--ContadorPositivo)";
+        }
+    });
+    //Gestionar el decremento del contador de clicks
+    Decrementar.addEventListener("click", function() {
+        Cantidad_Contador--;
+        Contador.innerText = Cantidad_Contador;
+        if(Cantidad_Contador === 0)
+        {
+            Contador.style.color = "var(--color-positivo)";
+        }
+        else if(Cantidad_Contador < 0)
+        {
+            Contador.style.color = "var(--ContadorNegativo)";
+        }
+    });
+
+    //Gestionar el reseteo del contador de clicks
+    Reset.addEventListener("click", function() {
+        Cantidad_Contador = 0;
+        Contador.innerText = Cantidad_Contador;
+        Contador.style.color = "var(--color-positivo)";
+    });
+}
+
+function Cronometro() {
+    var Iniciar = document.getElementById("start");
+    var Detener = document.getElementById("stop");
+    var hours = document.getElementById("hours");
+    var minutes = document.getElementById("minutes");
+    var seconds = document.getElementById("seconds");
+    var Reset = document.getElementById("reset");
+
+    var cronometro = null;
+    var h = 0;
+    var m = 0;
+    var s = 0;
+
+    //Gestionar el inicio del cronometro
+    Iniciar.addEventListener("click", function() {
+        Iniciar.disabled = true;
+        h = 0;
+        m = 0;
+        s = 0;
+        cronometro = setInterval(() => {
+            s++;
+            seconds.innerText = s;
+            if(s === 60){
+                s = 0;
+                m++;
+                minutes.innerText = m;
+            }
+            else if(m === 60){
+                m = 0;
+                h++;
+                hours.innerText = h;
+            }
+        }, 1000);
+    });
+    
+    //Gestionar el detención del cronometro
+    Detener.addEventListener("click", function() {
+        if(Detener.innerText === "Detener") 
+        {
+        Iniciar.disabled = false;
+        clearInterval(cronometro);
+        Detener.innerText = "Reanudar";
+        }
+        else 
+        {
+            Iniciar.disabled = true;
+            cronometro = setInterval(() => {
+                s++;
+                seconds.innerText = s;
+                if(s === 60){
+                    s = 0;
+                    m++;
+                    minutes.innerText = m;
+                }
+
+                if(m === 60){
+                    m = 0;
+                    h++;
+                    hours.innerText = h;
+                }
+            }, 1000);
+            Detener.innerText = "Detener";
+        }
+    });
+
+    //Gestionar el reseteo del cronometro
+    Reset.addEventListener("click", function() {
+        h = 0;
+        m = 0;
+        s = 0;
+        hours.innerText = h;
+        minutes.innerText = m;
+        seconds.innerText = s;
+        Detener.innerText = "Detener";
+        clearInterval(cronometro);
+        Iniciar.disabled = false;
+    });
+}
+
+function Reloj () {
+    var Day = document.getElementById("day");
+    var Today = document.getElementById("date");
+    var Hour = document.getElementById("hour");
+    var Minute = document.getElementById("minute");
+    var Second = document.getElementById("second");
+
+    //Gestionar lenguaje local
+    var userLang = navigator.language || 'es-ES';
+    
+    //Gestion de reloj
+    setInterval(() => {
+        actualizarReloj(Day,Today,Hour,Minute,Second,userLang);
+    },1000);
+}
+
+function actualizarReloj(Day,Today,Hour,Minute,Second,userLang) {
+    var date = new Date();
+    var DayName = date.toLocaleDateString(userLang, { weekday: 'long' });
+    var TodayFormatted = date.toLocaleDateString(userLang);
+
+    //Gestion de tiempo actual
+    var hours = date.getHours().toString().padStart(2, '0');
+    var minutes = date.getMinutes().toString().padStart(2, '0');
+    var seconds = date.getSeconds().toString().padStart(2, '0');
+
+    Day.innerText = DayName;
+    Today.innerText = TodayFormatted;
+    Hour.innerText = hours;
+    Minute.innerText = minutes;
+    Second.innerText = seconds;
+}
